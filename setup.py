2dbf58130f0df6f83b48f55e4283594d7cf5cefc
fequinteros <67396930+fequinteros@users.noreply.github.com>
2022-11-01 15:32:05 -0300

    Create setup.py
    patches for bell_checker to be installed

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    description = readme.read()
+
+setuptools.setup(
+    name='bell_checker',
+    version='0.0.1',
+    author='FeQuinteros',
+    description='A Qiskit module to construct and evaluate bell inequalities',
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/fequinteros/bell_checker',
+    packages=['bell_checker', 'threerra.discriminators'],
+    install_requires=['qiskit', 'numpy', 'scipy', 'matplotlib'],
+    include_package_data=True,
